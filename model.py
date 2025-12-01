73aac09086c21867a26000fb1bc20eb6fd04ff94
mnjkmrbb <mnjkmrbb@gmail.com>
2025-12-01 06:02:49 +0000

    Cleanup: Remove intermediate test files and build artifacts

--- model.py
+++
@@ -1,8 +0,0 @@
-from sklearn.linear_model import LogisticRegression
-
-def create_model():
-    """
-    Create and return a LogisticRegression model.
-    """
-    model = LogisticRegression()
-    return model
--- utils.py
+++
@@ -1,2 +0,0 @@
-# Helper functions can be added here as needed
-# Currently empty for this project
